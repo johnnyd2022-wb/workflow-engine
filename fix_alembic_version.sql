92c9d854873251c1d060e1ca236027a2068970c8
johnnyd105 <johnny@whistlebird.co.nz>
2026-01-04 21:48:27 +1300

    removing local alembic files

--- fix_alembic_version.sql
+++
@@ -1,8 +0,0 @@
--- Fix alembic_version table - should only have ONE row
--- Delete the old revision and keep only the current head
-
-DELETE FROM alembic_version WHERE version_num = 'acf7c513c15e';
-
--- Verify only the head remains
-SELECT * FROM alembic_version;
-
